6dca02cb50c0b182dc5938f4b1617af6392df4b4
adnanrony <adnanrony19@gmail.com>
2024-04-09 23:03:06 +0600

    connect mongodb

--- ph-01/module02/server/server.js
+++ ph-01/module02/server/server.js
@@ -1,29 +1,57 @@
 
 const http=require('http')
 const url=require("url")
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const fs=require('fs')
 const port=3000;
 const localhost="127.0.0.1"
 
 //json data
-const posts=[
-    {
-        "id": 1,
-        "title": "Mr.",
-        "name": "John Doe"
-    },
-    {
-        "id": 2,
-        "title": "Ms.",
-        "name": "Jane Smith"
-    },
-    {
-        "id": 3,
-        "title": "Dr.",
-        "name": "Michael Johnson"
-    }
-];
+// const posts=[
+//     {
+//         "id": 1,
+//         "title": "Mr.",
+//         "name": "John Doe"
+//     },
+//     {
+//         "id": 2,
+//         "title": "Ms.",
+//         "name": "Jane Smith"
+//     },
+//     {
+//         "id": 3,
+//         "title": "Dr.",
+//         "name": "Michael Johnson"
+//     }
+// ];
+
+const uri = "mongodb+srv://nodejs:nodejs@cluster0.gjcm0gd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+  
+    await client.connect();
+    const database=client.db("nodejs")
+
+    // create db collection
+
+    const postcollection=database.collection("post")
+   
+    await client.db("nodejs").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
 
+
+
+
+    
 const server=http.createServer((req,res)=>{
     console.log(req.url,req.method)
     
@@ -31,33 +59,50 @@ const server=http.createServer((req,res)=>{
 
     const paraseUrl=new URL(req.url,`htttp://${req.headers.host}`)
 
-    const pathname=paraseUrl.pathname
+const pathname=paraseUrl.pathname
 
     // console.log(pathname)
 
     if(pathname==='/home' && req.method==='GET' ){
+
+        fs.readFile(__dirname + '/index.html','utf-8',(err,data)=>{
+            if(err){
+                throw new Error("Error reading file ");
+            }
+            
         res.writeHead(200,{
-            "Content-type": "text/html"
+            'Content-type':"text/html"
         })
-        res.end(`
-        <ul>
-    <li><a href="default.asp">Home</a></li>
-    <li><a href="news.asp">News</a></li>
-    <li><a href="contact.asp">Contact</a></li>
-    <li><a href="about.asp">About</a></li>
-        </ul>
-        `)
+        res.end(data)
+        })
     }
     else if(pathname==='/posts' && req.method==="GET"){
-        
-        const query=paraseUrl.searchParams
-        const postid=query.get('id')
-        const expectedcode=posts.find((post)=>post.id==postid)
+
+        // const query=paraseUrl.searchParams
+        // const postid=query.get('id')
+        // const expectedcode=posts.find((post)=>post.id==postid)
+
+        fs.readFile(__dirname + '/post.json','utf-8',(err,data)=>{
+            if(err){
+                throw new Error("Error reading file ");
+            }
+            
         res.writeHead(200,{
             'Content-type':"application/json",
             "email":"adnanrony19@gmail.com"
         })
-        res.end(JSON.stringify(expectedcode))
+        res.end(data)
+        })
+        
+
+
+
+
+        // res.writeHead(200,{
+        //     'Content-type':"application/json",
+        //     "email":"adnanrony19@gmail.com"
+        // })
+        // res.end(JSON.stringify(expectedcode))
     }
     else if(pathname==='/blog' && req.method==='GET'){
         res.writeHead(200,{
@@ -77,3 +122,24 @@ const server=http.createServer((req,res)=>{
 server.listen(port,localhost,()=>{
     console.log(`server is on and port ${port} `)
 })
+
+  }
+   finally {
+   
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+
+
